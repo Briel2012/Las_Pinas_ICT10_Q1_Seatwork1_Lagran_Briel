9b7afedca21c9ae9d7f619fa0a29b84e40be4912
Briel2012 <lagran.ap@obmontessori.edu.ph>
2026-09-04 07:48:50 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+studname = "Marcus Araja" #string
+studage = 15 #integers
+stud_height = 167.5 #float
+country_to_visit = ["Switzerland", "France", "Japan"] #list
+stud_new = False #boolean
+stud_info = {
+    "color" : "green",
+    "car_brand" : "Honda",
+    "shoe_size": 9.5,
+    "bff" : "Marcus" 
+    } #dictionary 
+
+stud_favefruits = {
+    "Apple",
+    "Mango",
+    "Grape",
+    "Pinnaple",
+    "Watermelon", 
+} #set
+days_of_week = (
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ) #tuple
+
+print("Name:", studname)
+print("Age:", studage)
+print("Height:", stud_height)
